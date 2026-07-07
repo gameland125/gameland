e45ac960f065357656ee33e6cefea9dff5f9f145
gameland125 <farzad03x@gmail.com>
2026-07-07 21:45:11 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,19 @@
-const CACHE_NAME = "ps4-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/auto.html",
-  "/offline.html",
-  "/loader.js",
-  "/payloads/goldhen.bin"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('ps4-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/auto.html',
+        '/offline.html',
+        '/payloads/goldhen.bin'
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
